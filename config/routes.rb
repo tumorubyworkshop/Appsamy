b2bd21512af446a45509ea689d8f29f7a3a4f089
appsamy <knarik.ghazaryan.y@tumo.org>
2019-07-11 19:13:35 +0400

    Updsted a lot of stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+	root 'home#index'
+
+	resources :products
+	resources :cart_items, only: [:destroy]
+	get '/cart', to: 'carts#index'
+
+	get '/cart/add', to: 'carts#add'
 end
