53db55350e604752ca2861fd7f0a3562c2d18da9
appsamy <knarik.ghazaryan.y@tumo.org>
2019-07-12 18:48:35 +0400

    final

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,23 @@
 class Cart < ApplicationRecord
-	 has_many :cart_items
-	 belongs_to :user
 
-	 def total
-	 	cart_items.inject(0) do |sum, item|
-	 		sum + item.product.price
-	 	end
-	 end
+	has_many :cart_items
+	belongs_to :user
+
+	def subtotal
+		cart_items.inject(0) do |sum, item|
+			sum + item.product.price
+		end
+	end
+
+  def tax
+    subtotal * 0.1
+  end
+
+  def total
+    subtotal + tax
+  end
+
+  def checkout!
+    update(checked_out: true)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
 	root 'home#index'
 
 	resources :products
+
 	resources :cart_items, only: [:destroy]
+
 	get '/cart', to: 'carts#index'
 
 	get '/cart/add', to: 'carts#add'
+
+  	get '/cart/checkout', to: 'carts#checkout'
+
+  	get '/cart/:id', to: 'carts#show', as: 'carts'
 end
